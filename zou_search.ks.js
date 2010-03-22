fe7ec0b7cfb4a5f422a4bc1458ea53c00e025dad
basyura <basyura@gmail.com>
2010-03-22 13:43:44 +0900

    shell を使用した呼び出しに変更
    取り込み元
    http://keysnail.g.hatena.ne.jp/mooz/20100316

--- zou_search.ks.js
+++ zou_search.ks.js
@@ -3,18 +3,16 @@ var PLUGIN_INFO =
     <name>zou search</name>
     <name lang="ja">hamazou search</name>
     <description>hamazou and tabezou search plugin</description>
-    <description lang="ja">hamazou と tabezou で検索するプラグインです</description>
-    <version>0.0.1</version>
+    <description lang="ja">hamazou と tabezou で検索</description>
+    <version>0.0.2</version>
     <updateURL>http://github.com/basyura/zou-search/raw/master/zou_search.ks.js</updateURL>
     <iconURL>http://github.com/basyura/zou-search/raw/master/logo.jpg</iconURL>
     <author mail="basyura@gmail.com" homepage="http://github.com/basyura/zou-search">basyura</author>
     <license>The MIT License</license>
     <license lang="ja">MIT ライセンス</license>
-    <minVersion>1.0</minVersion>
+    <minVersion>1.5.0</minVersion>
     <include>main</include>
     <provides>
-        <ext>hamazou-search</ext>
-        <ext>tabezou-search</ext>
     </provides>
 	<options>
 		<option>
@@ -37,53 +35,52 @@ plugins.options["zou_search.user"] = "basyura"
 >||
 plugins.options["zou_search.user"] = "basyura"
 ||<
-キーを登録する。
+shell を呼び出すキーを登録する。
+>||
+key.setViewKey(':', function () {
+    shell.input();
+}, 'Command System');
+||<
+表示された入力画面にコマンドと検索したい単語を入力する。コマンドは tab で補完可。
+はまぞう検索コマンド : hamazou
+たべぞう検索コマンド : tabezou
+>||
+hamazou リファクタリング
+||<
+Enter で新しいタブに検索結果が表示される。バックグラウンドでタブを開きたい場合は ! をコマンドの最後に付加する。
+>||
+hamazou! リファクタリング
+||<
+コマンド入力が面倒な場合は、以下の様にあらかじめ入力した状態のショートカットキーを登録しておく。バックグランドでタブを開きたい場合は false を指定する。
 >||
 key.setViewKey('h', function (aEvent, aArg) {
-    ext.exec("hamazou-search", aArg);
+    shell.input("hamazou ");
 }, 'hamazou search', true);
-key.setViewKey('t', function (aEvent, aArg) {
-    ext.exec("tabezou-search", aArg);
-}, 'tabezou search', true);
 ||<
+	
+履歴：
+0.0.2 shell 呼び出しに変更 http://keysnail.g.hatena.ne.jp/mooz/20100316
 
+		
     ]]></detail>
 </KeySnailPlugin>;
 
-ext.add("hamazou-search", function (ev, arg) {
-	var user = plugins.options["zou_search.user"];
-	if(user == undefined) {
-		alert("you need to set zou_search.user option.");
-		return;
-	}
-	prompt.reader({
-		message    : "hamazou: ",
-		callback   : function (word) {
-			word = encodeURIComponent(word);
-			var url = "http://d.hatena.ne.jp/" 
-			                + user 
-							+ "/hamazou/search?mode=&ie=utf-8&word=" 
-							+ word;
-        	gBrowser.loadOneTab(url, null, null, null, true);
+function defineFooZouCommand(names, description, dir) {
+	shell.add(names, description,
+	function (args, extra) {
+		let words = encodeURIComponent(extra.left);
+		let url = "http://d.hatena.ne.jp/" + user + "/" + dir + "/search?mode=&ie=utf-8&word=" + words;
+		gBrowser.loadOneTab(url, null, null, null, extra.bang);
+	},
+	{
+		bang      : true,
+		literal   : 0,
+		completer : function (args, extra) {
+			let engines = [util.suggest.ss.getEngineByName("Google")];
+			return completer.fetch.suggest(engines, true)(extra.left || "", extra.whole || "");
 		}
 	});
-}, M({ja: "はまぞう検索", en: "hamazou search"}));
+}
 
-ext.add("tabezou-search", function (ev, arg) {
-	var user = plugins.options["zou_search.user"];
-	if(user == undefined) {
-		alert("you need to set zou_search.user option.");
-		return;
-	}
-	prompt.reader({
-		message    : "tabezou: ",
-		callback   : function (word) {
-			word = encodeURIComponent(word);
-			var url = "http://d.hatena.ne.jp/" 
-			                + user 
-							+ "/tabezou/search?mode=&ie=utf-8&word=" 
-							+ word;
-        	gBrowser.loadOneTab(url, null, null, null, true);
-		}
-	});
-}, M({ja: "たべぞう検索", en: "tabezou search"}));
+defineFooZouCommand(["hama[zou]"], M({ja: "はまぞう検索", en: "hamazou search"}), "hamazou");
+defineFooZouCommand(["tabe[zou]"], M({ja: "たべぞう検索", en: "tabezou search"}), "tabezou");
